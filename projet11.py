a7e7a0952f985eb05237b08f43323ff7df1057e8
Yassine Chouiti <yassine.chouiti@etu.univ-grenoble-alpes.fr>
2024-01-13 18:50:50 +0100

    Push Final Project status : Completed.

--- projet11.py
+++ projet11.py
@@ -4,18 +4,9 @@ Script: SAE105/projet11
 Création: chouitiy, le 14/11/2023
 """
 
-# Imports
-# from tools import *
-# import sys
-
-import matplotlib.pyplot as plt
-import matplotlib.table as Table
+import matplotlib.pyplot as mp
 from matplotlib.gridspec import GridSpec
 import numpy as np
-
-# import pandas as pd
-# import pandas as pds
-# import plotnine as pltn
 
 try:
     import tools_constantes
@@ -28,12 +19,13 @@ except ModuleNotFoundError:
 
 
 # Fonctions
-
 def calcule_nombre_minutes(heure):
     """
     Converti un temps donné en heure et minutes, en minutes.
-        :param heure (str): Heure au format "HH:MM"
-        :return (str): Nombre de minutes
+    Paramètres:
+        heure (str): Heure au format "HH:MM"
+    Retourne:
+        (str): Nombre de minutes
     """
     heures = int(heure.split(":")[0])
     minutes = int(heure.split(":")[1])
@@ -44,13 +36,16 @@ def calcule_nombre_minutes(heure):
 def calcule_duree(heure_debut, heure_fin):
     """
     Renvoi la durée d'un événement basé sur son heure de début et de fin; au format HH:MM
-        :param heure_debut (str): l'heure de début au format "HH:MM"
-        :param heure_fin (str): l'heure de fin au format "HH:MM"
-        :return (str):  la durée au format "HH:MM"
+    Paramètres:
+        heure_debut (str): l'heure de début au format "HH:MM"
+        heure_fin (str): l'heure de fin au format "HH:MM"
+    Retourne
+        (str):  la durée au format "HH:MM"
     """
     temps_en_minute = calcule_nombre_minutes(heure_fin) - calcule_nombre_minutes(heure_debut)
     heures = temps_en_minute // 60
     minutes = temps_en_minute % 60
+
     if heures < 10:
         heures = "0" + str(temps_en_minute // 60)
     if minutes < 10:
@@ -62,9 +57,11 @@ def calcule_duree(heure_debut, heure_fin):
 def compare_heures(heure1, heure2):
     """
     Renvoi 1, 0, ou 1 si heure1 est après, avant ou pareil que heure2 (respectivement).
-        :param heure1 (str): la première heure
-        :param heure2 (str): la seconde heure
-        :return (int): Résultat de comparaison
+    Paramètres:
+        heure1 (str): la première heure
+        heure2 (str): la seconde heure
+    Retourne:
+        (int): Résultat de comparaison
     """
     heure1_minute = calcule_nombre_minutes(heure1)
     heure2_minute = calcule_nombre_minutes(heure2)
@@ -80,11 +77,11 @@ def compare_heures(heure1, heure2):
 def compare_dates(date1, date2):
     """
     Vérifie si date1 est après, avant ou pareil que date2
-        Args:
+        Paramètres:
             date1 (str) : la première date
             date2 (str) : la deuxième date
 
-        Returns:
+        Retourne:
             (int) : Renvoi 1, -1 ou 0 selon si date1 est après, avant ou pareil que date2
     """
     if date1 == date2:
@@ -101,12 +98,12 @@ def compare_dates(date1, date2):
 def est_date_dans_intervalle(date, debut, fin):
     """
     Vérifie si une date est incluse dans une intervalle donnée.
-        Args:
+        Paramètres:
             date (str): la date à vérifier
             debut (str): la date de début d'intervalle (incluse)
             fin (str): la date de début d'intervalle (incluse)
 
-        Returns:
+        Retourne:
             (bool): Si la date est dans l'intervalle
     """
     if date == debut or date == fin:
@@ -121,16 +118,16 @@ def est_date_dans_intervalle(date, debut, fin):
 def recupere_champ_csv(evenement, nom):
     """
     Sélectionne et renvoi le champ "nom" dans l'évènement spécifié.
-        Args:
+        Paramètres:
             evenement (str): évènement au format csv
             nom (str): nom du champ spécifique
 
-        Returns:
+        Retourne:
                 (str): le champ associé à l'évènement
     """
     event_l = evenement.split(";")
-    csv_template = ["uid", "date", "debut|fin", "modules", "modalite", "evaluation", "theme", "salles", "profs",
-                    "groupes"]
+    csv_template = ["uid", "date", "debut|fin", "modules", "modalite", "evaluation", "theme", "salles", "profs", "groupes"]
+
     if nom == "heure":
         return event_l[2]
     elif nom == "debut":
@@ -146,14 +143,15 @@ def recupere_champ_csv(evenement, nom):
 def selectionne_SAE105_groupe(calendrier, groupe):
     """
     Extrait les évènements relatifs à la SAE105 auxquels assiste le groupe de TD "groupe".
-    Args:
+    Paramètres:
         calendrier (str): Une liste d'évènements ADE
         groupe (str): Un groupe de TD
 
-    Returns:
+    Retourne:
         (list of str): liste de cours de SAE105 suivie par le groupe de TD
     """
     event_list = []
+
     for event in calendrier:
         evenement = event.split(";")
         ressource = evenement[3].split("-")[0]  # [3] = champ ressource
@@ -167,14 +165,15 @@ def selectionne_SAE105_groupe(calendrier, groupe):
 def selectionne_ressources_groupe(calendrier, groupe):
     """
     Extrait les évènements auxquels assiste le groupe de TD "groupe".
-    Args:
+    Paramètres:
         calendrier (str): Une liste d'évènements ADE
         groupe (str): Un groupe de TD
 
-    Returns:
+    Retourne:
         (list of str): liste de ressource suivie par le groupe de TD
     """
     event_list = []
+
     for event in calendrier:
         evenement = event.split(";")
         group_event = evenement[-1]
@@ -187,10 +186,10 @@ def selectionne_ressources_groupe(calendrier, groupe):
 def deduit_annee_du_module(module):
     """
     Déduit l'année d'un module "module" à partir de son code.
-    Args:
+    Paramètres:
         module (str): Le module
 
-    Returns:
+    Retourne:
         (int or None): L'année (1, 2 ou 3) auquel à lieu le module OU None
     """
     if module == "Autre":
@@ -206,10 +205,10 @@ def deduit_annee_du_module(module):
 def deduit_annee_du_groupe(groupe):
     """
     Déduit l'année du groupe de TD.
-    Args:
+    Paramètres:
         groupe (str): Le groupe
 
-    Returns:
+    Retourne:
         (int or None): L'année (1, 2 ou 3) auquel appartient le groupe
     """
     return int(groupe[1])
@@ -218,14 +217,15 @@ def deduit_annee_du_groupe(groupe):
 def est_dans_competence_S1(module, competence):
     """
     Vérifie si un module fait bien parti du S1 ET de la compétence "competence".
-    Args:
+    Paramètres:
         module (str): Un module (code & diminutif)
         competence (str): Une compétence
 
-    Returns:
+    Retourne:
         fait_partie (bool): True si le module fait partie de la compétence, False sinon.
     """
     fait_partie = False
+
     if competence not in tools_constantes.COMPETENCES:
         return fait_partie
     elif "R" == module[0] and int(module[1]) != 1:  # Forme "RXYY" ou "RXcyYY"
@@ -234,9 +234,9 @@ def est_dans_competence_S1(module, competence):
         return fait_partie
     else:
         num_competence = int(competence[2])
-        for ressource in tools_constantes.COEFFS_S1:
-            if module in ressource[0]:
-                if ressource[num_competence] != 0:
+        for ressources in tools_constantes.COEFFS_S1:
+            if module in ressources[0]:
+                if ressources[num_competence] != 0:
                     fait_partie = True
 
         return fait_partie
@@ -245,15 +245,16 @@ def est_dans_competence_S1(module, competence):
 def selectionne_creneaux_groupe_competence(calendrier, groupe, competence):
     """
     Extrait la liste d'évènement auxquels participent le groupe de TD, selon un module d'une competence du S1.
-    Args:
+    Paramètres:
         calendrier (list of str): Liste d'évènement
         groupe (str): Groupe de TD
         competence (str): Compétence
 
-    Returns:
+    Retourne:
         (list of str): Liste d'évènement qui respecent les conditions de groupe et de coméptence.
     """
     event_list = []
+
     for event in calendrier:
         event_group = recupere_champ_csv(event, "groupes").split("|")
         if groupe in event_group:
@@ -266,9 +267,8 @@ def selectionne_creneaux_groupe_competence(calendrier, groupe, competence):
 def exemple_export_markdown():
     """
     Affiche un tableau markdown selon des données initialisées dans la fonction
-    Returns:
+    Retourne:
         None
-
     """
     entetes = ["Code", "Diminutif", "Discipline"]
     donnees = ["R102;ArchiRes;Réseaux", "R204;Téléphonie;Télécoms", "R107;Python1;Info"]
@@ -302,22 +302,25 @@ def exemple_export_markdown():
 
 def nb_heures_par_modalite(calendrier):
     """
-    Renvoi le nombre d'heure par modalité.
-    Args:
+    Renvoi le nombre d'heures par modalité.
+    Paramètres:
         calendrier (list of str): Liste d'événement sous forme de str
 
-    Returns:
+    Retourne:
         (list of floats): nombre d'heures par modalité (TP, CM etc.)
 
     """
     heures = [0, 0, 0, 0, 0]
+
     for event in calendrier:
         modalite = recupere_champ_csv(event, "modalite")
+
         h_debut = recupere_champ_csv(event, "debut")
         h_fin = recupere_champ_csv(event, "fin")
         duree = calcule_nombre_minutes(calcule_duree(h_debut, h_fin))
+
         heures[tools_constantes.MODALITES.index(modalite)] += duree
-        heures[-1] += duree
+        heures[-1] += duree  # Total
 
     return [(x / 60) for x in heures]
 
@@ -325,20 +328,22 @@ def nb_heures_par_modalite(calendrier):
 def repartition_moyenne_volume_horaire_competence(calendrier, competence):
     """
     Calcule la répartition moyenne des voluymes horaires par modalité des modules du S1 ayant trait à une compétence.
-    Args:
+    Paramètres:
         calendrier: Liste d'événement
         competence: Une compétence parmi 'RT1-Administrer', 'RT2-Connecter', 'RT3-Programmer'
 
-    Returns:
+    Retourne:
         (str): repartition moyenne du volume horaires des cours de la compétence
     """
 
     all_groups = [[0, 0, 0, 0, 0] for x in range(4)]
+
     for i in range(4):
         groupe = "B1G" + str(i + 1)
         for event in calendrier:
             module = recupere_champ_csv(event, "modules")
             if est_dans_competence_S1(module, competence) and groupe in recupere_champ_csv(event, "groupes"):
+
                 # Durée en minute
                 debut = recupere_champ_csv(event, "debut")
                 fin = recupere_champ_csv(event, "fin")
@@ -370,24 +375,23 @@ def repartition_moyenne_volume_horaire_competence(calendrier, competence):
 def traitement(calendrier):
     """
     Calcule pour les 3 compétences du S1 la répartition des volumes horaires par modalité, en donnant les moyennes par groupe.
-    Args:
+    Paramètres:
         calendrier: Liste d'événement
 
-    Returns:
+    Retourne:
         (list of str): Liste du rapport de volume horaire par compétence
     """
-    return [repartition_moyenne_volume_horaire_competence(calendrier, tools_constantes.COMPETENCES[x]) for x in
-            range(3)]
+    return [repartition_moyenne_volume_horaire_competence(calendrier, tools_constantes.COMPETENCES[x]) for x in range(3)]
 
 
 def export_markdown(resultats, entetes):
     """
     Affiche un tableau markdown selon les données passées en paramètre.
-    Args:
+    Paramètres:
         resultats: Liste des différents volumes horaires de chaque compétences
         entetes: Liste des titres de colonnes
 
-    Returns:
+    Retourne:
         None
     """
     donnees_split = [x.split(";") for x in resultats]
@@ -417,89 +421,77 @@ def export_markdown(resultats, entetes):
 
     print("")
 
-
-# def export_png(resultats):
-#     """
-#     Affiche une graphique présentant la répartion du volume horaire selon chaque modalité pour chaque compétence.
-#     Args:
-#         resultats (list of str): Volumes horaires par compétence
-#
-#     Returns:
-#         None
-#     """
-#     x = np.array(tools_constantes.COMPETENCES)
-#     proj_y = np.array([float(x.split(";")[-2]) for x in resultats])
-#     tp_y = np.array([float(x.split(";")[-3]) for x in resultats])
-#     td_y = np.array([float(x.split(";")[-4]) for x in resultats])
-#     cm_y = np.array([float(x.split(";")[-5]) for x in resultats])
-#
-#     mp.bar(x, cm_y, color='#5D70C1')
-#     mp.bar(x, tp_y, bottom=cm_y, color='#2A2E46')
-#     mp.bar(x, td_y, bottom=tp_y, color='#E84E0F')
-#     mp.bar(x, proj_y, bottom=td_y+tp_y, color='#FFE6DF')
-#     mp.title("Volumes horaires au S1")
-#
-#     mp.savefig("figure.png")
-#     mp.show()
-
-
 def export_png(resultats):
     """
     Affiche une graphique présentant la répartion du volume horaire selon chaque modalité pour chaque compétence.
-    Args:
+    Paramètres:
         resultats (list of str): Volumes horaires par compétence
 
-    Returns:
+    Retourne:
         None
     """
-    # Vos données existantes
-    # Vos données existantes
     x = np.array(tools_constantes.COMPETENCES)
     proj_y = np.array([float(x.split(";")[-2]) for x in resultats])
     tp_y = np.array([float(x.split(";")[-3]) for x in resultats])
     td_y = np.array([float(x.split(";")[-4]) for x in resultats])
     cm_y = np.array([float(x.split(";")[-5]) for x in resultats])
 
-    fig, ax = plt.subplots()
+    # Grille pour l'affichage du tableau en dessous du graph
+    fig = mp.figure(figsize=(8, 6))
+    gs = GridSpec(2, 1, height_ratios=[3, 1])
 
-    # Tracer le diagramme à colonnes empilées
-    ax.bar(x, cm_y, color='#5D70C1', label='CM')
-    ax.bar(x, tp_y, bottom=cm_y, color='#2A2E46', label='TP')
-    ax.bar(x, td_y, bottom=tp_y, color='#E84E0F', label='TD')
-    ax.bar(x, proj_y, bottom=td_y + tp_y, color='#FFE6DF', label='Projets')
+    # Graphique à colonnes empilées
+    graph = fig.add_subplot(gs[0])
+    graph.bar(x, cm_y, color='#5D70C1', label='CM', edgecolor="#575757", linewidth=0.6)
+    graph.bar(x, tp_y, bottom=cm_y, color='#2A2E46', label='TP', edgecolor="#575757", linewidth=0.6)
+    graph.bar(x, td_y, bottom=tp_y, color='#E84E0F', label='TD', edgecolor="#575757", linewidth=0.6)
+    graph.bar(x, proj_y, bottom=td_y + tp_y, color='#FFE6DF', label='Projets', edgecolor="#575757", linewidth=0.6)
+    graph.legend()
+    graph.set_title("Volumes horaires au S1")
 
-    ax.legend()
-    ax.set_title("Volumes horaires au S1")
-
-    # Ajouter le tableau en dessous du graphique
-    table_data = [[" ", " ", " ", " "],
+    table_data = [["", x[0], x[1], x[2]],
                   ["Proj", proj_y[0], proj_y[1], proj_y[2]],
-                  ["TD", tp_y[0], tp_y[1], tp_y[2]],
-                  ["TP", td_y[0], td_y[1], td_y[2]],
+                  ["TD", td_y[0], td_y[1], td_y[2]],
+                  ["TP", tp_y[0], tp_y[1], tp_y[2]],
                   ["CM", cm_y[0], cm_y[1], cm_y[2]]]
 
-    table = Table.table(ax, cellText=table_data, loc='bottom', colWidths=[0.2] * len(table_data[0]))
-    table.scale(1.3, 1.5)  # Ajustez la taille du tableau selon vos besoins
+    # Ajoute le tableau EN DESSOUS du graph
+    tableau = fig.add_subplot(gs[1])
+    table = tableau.table(cellText=table_data, loc="center")
 
-    ax.add_table(table)
+    # Enlever les bords de la 1ère cellule + minor changes
+    table[0, 0].visible_edges = "BR"
+    table.auto_set_font_size(False)
+    table.set_fontsize(9)
+    table.scale(1, 1.5)
+    tableau.axis("off")
 
-    plt.savefig("figure.png")
-    plt.show()
+    # Couleurs des entêtes + couleur police
+    table[0, 1].set_facecolor("#8C8C8C")
+    table[0, 1].set_text_props(color="white")
+    table[0, 2].set_facecolor("#8C8C8C")
+    table[0, 2].set_text_props(color="white")
+    table[0, 3].set_facecolor("#8C8C8C")
+    table[0, 3].set_text_props(color="white")
 
+    # Couleurs des modalités + couleur police
+    table[1, 0].set_facecolor("#FFE6DF")  # Proj
+    table[2, 0].set_facecolor("#E84E0F")  # TD
+    table[3, 0].set_facecolor("#2A2E46")  # TP
+    table[3, 0].set_text_props(color="white")
+    table[4, 0].set_facecolor("#5D70C1")  # CM
+    table[4, 0].set_text_props(color="white")
 
-# def export_png_bis(resultats):
-#     events = [x.split(";") for x in resultats]
-#     data = {'Compétence': {0: 'RT1-Administrer', 1: 'RT2-Connecter', 2: 'RT3-Programmer'},
-#         'RT1-Administrer': {'CM': events[0][1], 'TD':events[0][2], 'TP': events[0][3], 'Proj': events[0][4]},
-#             'RT2-Connecter': {'CM': events[1][1], 'TD': events[1][2], 'TP': events[1][3], 'Proj': events[1][4]},
-#             'RT3-Programmer': {'CM': events[2][1], 'TD': events[2][2], 'TP': events[2][3], 'Proj': events[2][4]}}
-#
-#     df = pd.DataFrame(data)
-#     df_melt = pd.melt(df, id_vars=['Compétence'], value_vars=['RT1-Administrer', 'RT2-Connecter', 'RT3-Programmer'])
-#     print(df_melt)
-#
-#     #print(df)
+    # Pour suivre un ordre logique sur la légende
+    clrs, titles = graph.get_legend_handles_labels()
+    graph.legend(reversed(clrs), reversed(titles))
 
+    # Rajouter les lignes des ordonnées
+    for y_value in np.array([0, 50, 100, 150, 200, 250, 300]):
+        graph.axhline(y_value, color='#CCCCCC', linestyle='--', linewidth=0.8)
+
+    mp.savefig("figure.png")
+    mp.show()
 
 # Programme principal
 def main():
@@ -509,7 +501,6 @@ def main():
 
     export_markdown(resultats, ["COMPETENCES", "CM", "TD", "TP", "Projet"])
     export_png(resultats)
-    # export_png_bis(resultats)
 
 
 if __name__ == '__main__':
